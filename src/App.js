074f49e5c3621bab05527491ab1a77e78a1cb6b7
Muya, Anastacia Okhuya <muyaanastacia@gmail.com>
2026-02-04 20:04:28 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-const Pizza = () => {
-    return React.createElement("div" ,{}, [
-      React.createElement("h1", {}, "The pepperoni Pizza" ),
-        React.createElement("p", {}, "Mozzarella Cheese, Pepperoni"),
-    ]);
+const Pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
 };
+
 const App = () => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, "Padre Gino's"),
-        React.createElement(Pizza),
-        React.createElement(Pizza),
-        React.createElement(Pizza),
-        React.createElement(Pizza),
-        React.createElement(Pizza),
-    ]);
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Pixel Perfect Pizzas"),
+    React.createElement(Pizza, {
+      name: "The Pepperoni Pizza",
+      description: "Mozzarella Cheese, Pepperoni",
+    }),
+    React.createElement(Pizza, {
+      name: "The Hawaiian Pizza",
+      description: "Sliced Ham, Pineapple, Mozzarella Cheese",
+    }),
+    React.createElement(Pizza, {
+      name: "The Big Meat Pizza",
+      description: "Bacon, Pepperoni, Italian Sausage, Chorizo Sausage",
+    }),
+  ]);
 };
+
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(React.createElement(App)); 
+root.render(React.createElement(App));
