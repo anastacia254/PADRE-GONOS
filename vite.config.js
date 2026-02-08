3b7d928dd32141452e77c699785313eb4c4767bc
Noella Muya <muyaanastacia@gmail.com>
2026-02-08 22:24:40 +0300

    ....

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,17 @@ import { defineConfig } from "vite";
 import React from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [React()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+      "/public": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
+  },
+  plugins: [react()],
 });
