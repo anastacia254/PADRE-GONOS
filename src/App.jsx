fe5efbe28795365b2519850a122a37be163b6d58
Anastacia Muya <amuya@kippra.or.ke>
2026-02-10 15:16:20 +0300

    fixed error related to display

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import Pizza from "./Pizza.jsx";
+import "./style.css";
 
-// delete the Pizza component
 
 const App = () => {
   return (
@@ -9,7 +9,7 @@ const App = () => {
       <h1>Padre Gino's Pizza – Order Now</h1>
       <Pizza name="Pepperoni" description="Mozzarella Cheese, Pepperoni" image={"/public/pizzas/pepperoni.webp"} />
       <Pizza name="The Hawaiian Pizza" description="Sliced Ham, Pineapple, Mozzarella Cheese" image={"/public/pizzas/hawaiian.webp"} />
-      <Pizza name="The Big Meat Pizza" description="Bacon, Pepperoni, Italian Sausage, Chorizo Sausage" image={"/public/pizzas/bigmeat.webp"} />
+      <Pizza name="The Big Meat Pizza" description="Bacon, Pepperoni, Italian Sausage, Chorizo Sausage" image={"/public/pizzas/big_meat.webp"} />
     </div>
   );
 };
--- src/Pizza.jsx
+++ src/Pizza.jsx
@@ -1,7 +1,11 @@
+let counter = 0
 const Pizza = (props) => {
+
+  counter = Date.now();
+
   return (
-    <div className="Pizza">
-      <h1>{props.name}</h1>
+    <div className="pizza" onClick={() => console.log("Pizza Clicked", props.name)}>
+      <h1>{props.name} {counter}</h1>
       <p>{props.description}</p>
       <img src={props.image} alt={props.name} />
     </div>
