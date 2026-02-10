2c5c4a088563dceb467420e16459f03f335e9962
Anastacia Muya <amuya@kippra.or.ke>
2026-02-10 15:23:47 +0300

    added file

--- src/Pizza.jsx
+++ src/Pizza.jsx
@@ -1,11 +1,9 @@
-let counter = 0
 const Pizza = (props) => {
 
-  counter = Date.now();
 
   return (
     <div className="pizza" onClick={() => console.log("Pizza Clicked", props.name)}>
-      <h1>{props.name} {counter}</h1>
+      <h1>{props.name}</h1>
       <p>{props.description}</p>
       <img src={props.image} alt={props.name} />
     </div>
