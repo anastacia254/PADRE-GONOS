e9270c75232065026f4ce49bd144c6e104de50fd
Anastacia Muya <muyaanastacia@gmail.com>
2026-02-13 16:08:44 +0300

    changes

--- src/PizzaOfTheDay.jsx
+++ src/PizzaOfTheDay.jsx
@@ -16,6 +16,20 @@ const PizzaOfTheDay = () => {
     <div className="pizza-of-the-day">
       <h2>Pizza of the Day</h2>
       <div className="pizza-of-th-day-info">
-        <h3>{pizza.name}</h3>
+        <h3>{pizzaOfTheDay.name}</h3>
+        <p>{pizzaOfTheDay.description}</p>
+        <p className="pizza-of-the-day-price">
+            from: {intl.format(pizzaOfTheDay.sizes.S)}
+        </p>
       </div>
-  )
+      <img 
+      className="pizza-of-the-day-image"
+      src={pizzaOfTheDay.image} 
+      alt={pizzaOfTheDay.name} 
+      />
+      </div>
+      </div>
+  );
+};
+
+export default PizzaOfTheDay;
