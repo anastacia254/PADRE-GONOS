5a8d8fc8685591dc1f7486276d4989294859ea36
Anastacia Muya <muyaanastacia@gmail.com>
2026-02-16 14:51:45 +0300

    created contexts

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,10 @@
 export default function Header() {
     return (
-        <header>
+        <nav>
             <h1 className="logo">Padre Gino's Pizza</h1>
             <div className="nav-cart">
-                <span className="nav-cart-number">5</span>
+                🛒<span className="nav-cart-number">5</span>
             </div>
-        </header>
+        </nav>
     );
 }
